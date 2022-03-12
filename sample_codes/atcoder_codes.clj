154c7174be3ca7ce8a6e30c74ee445ab3ce25d16
matsutomo81 <87012577+matsutomo81@users.noreply.github.com>
2022-03-12 08:32:35 +0000

    add atcoder problems

--- sample_codes/atcoder_codes.clj
+++ sample_codes/atcoder_codes.clj
@@ -1,9 +1,16 @@
 ;https://atcoder.jp/contests/abc081/tasks/abc081_a
+(defn odd_or_even [a b]
+  (if (even? (* a b)) "Even" "Odd"))
+(println (odd_or_even 3 1))
+
+
+;https://atcoder.jp/contests/abc081/tasks/abc081_a
 (defn count_1 [s] (count (filter #(= \1 %) s)))
 
 (println(count_1 "101"))
 
 ;false
+;char型とstring型の違い
 (println (= "1" \1))
 
 ;true
